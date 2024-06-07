2f822635c11b30ff2ebbd0b1efb55868b5e24e4d
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-07 15:14:05 -0300

    Update global layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { GithubIcon } from "./components/GithubIcon";
+import { TwitterXIcon } from "./components/TwitterXIcon";
+import { LinkedinIcon } from "./components/LinkedinIcon";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Your Github Repos",
@@ -12,7 +16,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body>
+        <header className="flex flex-row-reverse bg-purple-600 p-4">
+          <nav className="flex gap-4">
+            <Link href="https://github.com/fernandobarrosd">
+              <GithubIcon/>
+            </Link>
+            <Link href="https://x.com/fbarrosdev">
+              <TwitterXIcon/>
+            </Link>
+
+            <Link href="https://www.linkedin.com/in/fernando-de-barros-204864241/">
+              <LinkedinIcon/>
+            </Link>
+          </nav>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
