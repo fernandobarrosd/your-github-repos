34f9a3703cd6e520d54c8814fdf87be721750b73
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-09 16:17:38 -0300

    Update repos page

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -1,11 +1,9 @@
 import { getServerSession } from "next-auth";
-import { SearchIcon } from "../components/SearchIcon";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
-import { GithubIcon } from "../components/GithubIcon";
-import Link from "next/link";
 import { GithubRepositoryType } from "@/types/GithubRepositoryType";
-import { GithubRepositoryItem } from "./components/GithubRepositoryItem";
+import { GithubReposList } from "./components/GithubReposList";
+
 
 export default async function Repos() {
     const session = await getServerSession(authOptions);
@@ -19,20 +17,9 @@ export default async function Repos() {
     });
     const repos = await response.json() as GithubRepositoryType[];
 
-
     return (
-        <main className="mt-4 flex w-full flex-col items-center">
-            <div className="flex w-72 border items-center justify-center rounded-2xl
-            bg-gray-100 p-0.5">
-                <input type="text" placeholder="Search"
-                className="w-72 px-3 bg-gray-100 text-[0.8rem]"/>
-                <SearchIcon className="mr-2"/>
-            </div>
-            <ul className="mt-8 grid grid-cols-2 gap-x-6 gap-y-8 mb-8">
-                {repos.map(({ id, name } ) => (
-                    <GithubRepositoryItem key={id} name={name}/>
-                ))}
-            </ul>
+        <main className="mt-4 flex max-w-full flex-col items-center">
+            <GithubReposList initialRepos={repos}/>
         </main>
     )
 }
