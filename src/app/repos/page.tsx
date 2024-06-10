4582451388ffb9253481700be6aa19b4bd00edb8
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-10 13:49:55 -0300

    Update repos page

--- src/app/repos/page.tsx
+++ src/app/repos/page.tsx
@@ -8,14 +8,15 @@ import { GithubReposList } from "./components/GithubReposList";
 export default async function Repos() {
     const session = await getServerSession(authOptions);
     if (!session) {
-        return redirect("/")
+        return redirect("/");
     }
-    const response = await fetch("https://api.github.com/user/repos", {
+    const response = await fetch("https://api.github.com/user/repos?per_page=20&page=1", {
         headers: {
             "Authorization": `Bearer ${session.accessToken}`
         }
     });
     const repos = await response.json() as GithubRepositoryType[];
+    
 
     return (
         <main className="mt-4 flex max-w-full flex-col items-center">
