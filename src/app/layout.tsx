8b1909b5984e378a75f8b9ceeb80b666c72edb52
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-12 16:14:19 -0300

    Update global layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body className="h-full">
         <Header/>
         <Providers>
           {children}
