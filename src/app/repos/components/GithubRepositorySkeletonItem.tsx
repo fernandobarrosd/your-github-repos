ea9735e8b2044ea2652871475e2f394e1dd65347
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-12 12:08:28 -0300

    Create GithubRepositorySkeletonItem skeleton component

---
+++ src/app/repos/components/GithubRepositorySkeletonItem.tsx
@@ -0,0 +1,13 @@
+export function GithubRepositorySkeletonItem() {
+    return (
+        <li className="w-32 flex flex-col items-center">
+            <div className="bg-gray-200 h-20 flex justify-center items-center 
+            rounded-md w-full"/>
+            <p className="mt-2 text-xs w-24 text-center
+            h-6 bg-gray-200"/>
+            <span 
+            className="text-xs bg-gray-200 mt-2 p-2 rounded-md
+             text-white font-bold w-24 h-8"/>
+        </li>
+    )
+}
