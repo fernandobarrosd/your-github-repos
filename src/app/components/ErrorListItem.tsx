eb377e7267b7cf88050785b4a95758679a28c37f
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-12 16:44:55 -0300

    Remove repos error page

--- src/app/components/ErrorListItem.tsx
+++
@@ -1,7 +0,0 @@
-export function ErrorListItem({ message } : { message: string }) {
-    return (
-        <li className="text-xs p-2 bg-purple-600 text-white rounded-md">
-            {message}
-        </li>
-    )
-}
