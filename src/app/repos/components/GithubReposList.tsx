5c8adb7636140bd1e7b3961130a2b4433255a8fc
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-12 12:09:17 -0300

    Update GithubReposList component

--- src/app/repos/components/GithubReposList.tsx
+++ src/app/repos/components/GithubReposList.tsx
@@ -5,6 +5,8 @@ import { GithubRepositoryItem } from "./GithubRepositoryItem";
 import { useState } from "react";
 import { GithubRepositoryType } from "@/types/GithubRepositoryType";
 import { useSession } from "next-auth/react";
+import { GITHUB_BASE_URL } from "@/variables";
+import { GithubRepositorySkeletonItem } from "./GithubRepositorySkeletonItem";
 
 type GithubReposListProps = {
     initialRepos: GithubRepositoryType[]
@@ -13,35 +15,42 @@ type GithubReposListProps = {
 export function GithubReposList({ initialRepos } : GithubReposListProps) {
     const [ repos, setRepos ] = useState<GithubRepositoryType[]>(initialRepos);
     const { data: session } = useSession();
+    const [ isLoading, setIsLoading ] = useState(false);
 
     function handleSearchGithubRepo(formData: FormData) {
         const searchValue = formData.get("search-value")?.toString();
 
         if (searchValue?.length) {
-            fetch(`https://api.github.com/search/repositories?q=${searchValue}&per_page=20&page=1`, {
+            setIsLoading(true);
+            fetch(`${GITHUB_BASE_URL}/search/repositories?q=${searchValue}&per_page=20&page=1`, {
                 headers: {
                     "Authorization": `Bearer ${session?.accessToken}`
                 }
             })
             .then(response => response.json())
-            .then(result => setRepos(result.items));
+            .then(result => setRepos(result.items))
+            .finally(() => setIsLoading(false));
         }
     }
-    async function handleInputChange(inputValue: string) {
+    function handleInputChange(inputValue: string) {
         if (!inputValue.length) {
-            const response = await fetch("https://api.github.com/user/repos?per_page=20&page=1", {
+            setIsLoading(true);
+
+            fetch(`${GITHUB_BASE_URL}/user/repos?per_page=20&page=1`, {
                 headers: {
                     "Authorization": `Bearer ${session?.accessToken}`
                 }
-            });
-            const repos = await response.json() as GithubRepositoryType[];
-            setRepos(repos);
+            })
+            .then(response => response.json())
+            .then(repos => setRepos(repos))
+            .finally(() => setIsLoading(false));
         }
     }
     return (
         <>
-        <form className="flex min-w-[300px] items-center justify-center rounded-2xl
-        bg-gray-200 p-1.5 sm:min-w-[700px] ring-[3.2px] ring-transparent has-[input:focus]:ring-purple-600"
+        <form className="flex items-center justify-center rounded-2xl
+        bg-gray-200 p-1.5 w-[300px] sm:w-[500px] lg:w-[800px]
+        ring-[3.2px] ring-transparent has-[input:focus]:ring-purple-600"
         action={handleSearchGithubRepo}>
            <input type="text" placeholder="Search"
            className="w-full px-3 bg-gray-200 text-[0.8rem] outline-none"
@@ -51,15 +60,40 @@ export function GithubReposList({ initialRepos } : GithubReposListProps) {
                 <SearchIcon className="mr-2"/>
             </button>
         </form>
-        <ul className="mt-12 grid grid-cols-2 gap-x-6 gap-y-8 mb-8
-        sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8">
-            {repos.map(({ id, full_name} ) => (
+        {isLoading ? (
+            <ul className="mt-12 grid
+            grid-cols-2 min-[550px]:grid-cols-3
+            md:grid-cols-4
+            lg:grid-cols-5
+            gap-x-6 gap-y-8 mb-8">
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+                <GithubRepositorySkeletonItem/>
+            </ul>
+        ) : (
+            <ul className="mt-12 grid
+            grid-cols-2 min-[550px]:grid-cols-3
+            md:grid-cols-4
+            lg:grid-cols-5
+            gap-x-6 gap-y-8 mb-8">
+            {repos.map(({ id, full_name, owner } ) => (
                 <GithubRepositoryItem
                 key={id}
+                ownerImage={owner.avatar_url}
                 fullName={full_name}
                 />
             ))}
         </ul>
+        )}
         </>
     )
 }
