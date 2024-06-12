2b395e336d0f2f26fe3b2332fdf3f14c3df15528
Fernando de Barros <fdebarros0910-2004@hotmail.com>
2024-06-12 16:27:06 -0300

    Create authOptions file

---
+++ src/app/api/auth/authOptions.ts
@@ -0,0 +1,25 @@
+import { AuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+
+export const authOptions : AuthOptions = {
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID!,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+        })
+    ],
+    callbacks: {
+        async jwt({ account, token }) {
+            if (account?.access_token) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token.accessToken) {
+                session.accessToken = token.accessToken;
+            }
+            return session;
+        }
+    }
+};
